a90df1be69e863c3bb4abfa116f85374210f3fc6
geoenterprise <jovannyrey@hotmail.com>
2025-09-13 00:43:56 -0500

    changing to .Net 8 and adding MAUI

---
+++ MobileApp/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Linq;
+
+namespace MobileApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private int _count;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        _count++;
+        CounterBtn.Text = $"Clicked {_count} times";
+    }
+
+    private async void OnGetWeatherClicked(object sender, EventArgs e)
+    {
+        try
+        {
+#if ANDROID
+            var baseAddress = "http://10.0.2.2:5022/";  // my API's HTTP port
+#else
+            var baseAddress = "https://localhost:7022/"; // my API's HTTPS port on Windows
+#endif
+            using var client = new HttpClient { BaseAddress = new Uri(baseAddress) };
+
+            var data = await client.GetFromJsonAsync<IEnumerable<WeatherForecast>>("weatherforecast");
+            if (data != null)
+            {
+                ResultLabel.Text = string.Join(Environment.NewLine,
+                    data.Select(w => $"{w.Date:d}: {w.TemperatureC}°C, {w.Summary}"));
+            }
+        }
+        catch (Exception ex)
+        {
+            ResultLabel.Text = $"Error: {ex.Message}";
+        }
+    }
+}
+
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+}
+
+
+
+
+//namespace MobileApp
+//{
+//    public partial class MainPage : ContentPage
+//    {
+//        int count = 0;
+
+//        public MainPage()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void OnCounterClicked(object sender, EventArgs e)
+//        {
+//            count++;
+
+//            if (count == 1)
+//                CounterBtn.Text = $"Clicked {count} time";
+//            else
+//                CounterBtn.Text = $"Clicked {count} times";
+
+//            SemanticScreenReader.Announce(CounterBtn.Text);
+//        }
+//    }
+
+//}
---
+++ MobileApp/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿namespace MobileApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder()
+            .UseMauiApp<App>();
+
+        return builder.Build();
+    }
+}
+
+
+//using Microsoft.Extensions.Logging;
+
+//namespace MobileApp
+//{
+//    public static class MauiProgram
+//    {
+//        public static MauiApp CreateMauiApp()
+//        {
+//            var builder = MauiApp.CreateBuilder();
+//            builder
+//                .UseMauiApp<App>()
+//                .ConfigureFonts(fonts =>
+//                {
+//                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+//                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+//                });
+
+//#if DEBUG
+//    		builder.Logging.AddDebug();
+//#endif
+
+//            return builder.Build();
+//        }
+//    }
+//}
