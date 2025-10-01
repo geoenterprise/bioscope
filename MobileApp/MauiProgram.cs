828edafa10d886141c42505a4f08e32c766be6d0
mictlantecutli <par21078@byui.edu>
2025-09-30 23:21:19 -0600

    Clean code into the MauiProgram file

--- MobileApp/MauiProgram.cs
+++ MobileApp/MauiProgram.cs
@@ -12,47 +12,6 @@ public static class MauiProgram
             .UseMauiApp<App>();
 
         return builder.Build();
-
-
-    #if ANDROID
-            var baseUri = new Uri("https://localhost:7071/");
-    #else
-            var baseUri = new Uri("https://localhost:7071/");
-    #endif
-
-        builder.Services.AddHttpClient<OrganismApiService>(c =>
-        {
-            c.BaseAddress = baseUri;
-        });
-
-        builder.Services.AddHttpClient();
-
-        return builder.Build();
     }
 }
 
-//using Microsoft.Extensions.Logging;
-
-//namespace MobileApp
-//{
-//    public static class MauiProgram
-//    {
-//        public static MauiApp CreateMauiApp()
-//        {
-//            var builder = MauiApp.CreateBuilder();
-//            builder
-//                .UseMauiApp<App>()
-//                .ConfigureFonts(fonts =>
-//                {
-//                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-//                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-//                });
-
-//#if DEBUG
-//    		builder.Logging.AddDebug();
-//#endif
-
-//            return builder.Build();
-//        }
-//    }
-//}
