2aa5db37c6362b7fc004cfc806907e4bd7a4f174
mictlantecutli <par21078@byui.edu>
2025-10-04 00:17:37 -0600

    Get the JSON suggestionS from API and usable fields for the suggestion or identification cards

--- MobileApp/Pages/SuggestionsPage.xaml.cs
+++ MobileApp/Pages/SuggestionsPage.xaml.cs
@@ -2,19 +2,61 @@
 using System.Net.Http.Json;
 using System.Linq;
 using System.Net.Http.Headers;
-using System.Text.Json; 
+using System.Text.Json;
+using MobileApp.Models;
+using System.Text.Json.Serialization;
+
 
 
 namespace MobileApp;
 
 public partial class SuggestionsPage : ContentPage
 {
+    
     public SuggestionsPage(string json)
     {
         InitializeComponent();
 
-        // Show raw JSON in the label
-        JsonLabel.Text = json;
+        try
+        {
+            var root = JsonDocument.Parse(json).RootElement;
+            var result = root.GetProperty("result");
+            var suggestionsJson = result.GetProperty("suggestions").GetRawText();
+            var suggestions = JsonSerializer.Deserialize<List<Suggestion>>(suggestionsJson);
+
+            
+            if (suggestions != null)
+            {
+                SuggestionsCollection.ItemsSource = suggestions;
+            }
+            
+            else
+            {
+                WarningLabel.Text = "We did not find any suggestion.";
+              
+
+            }
+
+
+
+            // foreach (var suggestion in suggestions)
+            // {
+            //     var name = suggestion.GetProperty("plant_name").GetString();
+            //     var prob = suggestion.GetProperty("probability").GetDouble();
+            //     Console.WriteLine($"{name} - {prob}");
+            // }
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+
+        }
+
+        
+    
     }
 
 }
